d90414f03ce42134099f2b1743c38be9bcf7f54f
Rafael Guimarães <rafaelguimaraeswm@gmail.com>
2019-08-28 21:17:48 -0300

    Add initial typeorm config

--- app.ts
+++ app.ts
@@ -1,23 +1,31 @@
+import "reflect-metadata"
 import express from "express"
 import morgan from "morgan"
 import errorHandler from "errorhandler"
 import bodyParser from "body-parser"
+import { createConnection } from "typeorm"
 
 import routes from "./routes"
 
-const port = process.env.PORT || 8080
-const app: express.Application = express()
+async function bootstrapApp() {
+    const port = process.env.PORT || 8080
+    const app: express.Application = express()
 
-if (app.get("env") === "development") {
-    app.use(errorHandler())
+    if (app.get("env") === "development") {
+        app.use(errorHandler())
+    }
+
+    app.use(morgan("dev"))
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+
+    app.use(routes)
+
+    app.listen(port, function() {
+        console.log("Listening on port " + port)
+    })
 }
 
-app.use(morgan("dev"))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(routes)
-
-app.listen(port, function() {
-    console.log("Listening on port " + port)
-})
+createConnection()
+    .then(bootstrapApp)
+    .catch(error => console.log(error))
